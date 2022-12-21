32939092aa7427926970f97f669d5c3ed24836e1
hodal66 <mhthodol@gmail.com>
2022-12-21 08:45:22 +0200

    Add mongoose configuratuion

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import express from "express";
+import cors from "cors";
+import { connect } from "./app/config/db.config.js";
 const server = express();
 const port = 9000;
+
+
+let corsOption = {
+    origin:`http://localhost:${port}`
+}
+server.use(cors(corsOption))
+server.use(express.json());
+server.use(express.urlencoded({extended:true}));
+
 server.use("/",(req,res)=>{
     res.send("Welcome to smart server door lock");
     console.log("Welcome to smart server door lock")
 })
+
+connect().then(()=>console.log("Mongo db is running Successfully"))
 
 server.listen(port, console.log(`Server is running at http://localhost:${port}/`));
